b7381ae50a53de36ed3d15d108ac087018ba08ef
Alon Emanuel <alon@alonemanuel.com>
2020-04-05 12:58:23 +0300

    Done stage 2. Rotate towards works. Plus added a small test

--- ex01_motion_tracking/Assets/CharacterAnimator.cs
+++ ex01_motion_tracking/Assets/CharacterAnimator.cs
@@ -17,13 +17,56 @@ public class CharacterAnimator : MonoBehaviour
         BVHParser parser = new BVHParser();
         data = parser.Parse(BVHFile);
         CreateJoint(data.rootJoint, Vector3.zero);
+
+        TestRotateTowardsVector();
+    }
+
+    private void TestRotateTowardsVector()
+    {
+        Debug.Log("Now testing");
+        List<Vector3> testVectors = new List<Vector3>();
+        testVectors.Add(new Vector3(1, 1, 1));
+        testVectors.Add(new Vector3(1, 2, 3));
+        testVectors.Add(new Vector3(4, 2, 0));
+        testVectors.Add(new Vector3(6, 9, 2));
+
+        foreach (Vector3 testV in testVectors)
+        {
+            Vector3 testVNorm = testV.normalized;
+            Debug.Log("NormtestV");
+            Debug.Log(testVNorm);
+            Matrix4x4 r = RotateTowardsVector(testV);
+            Debug.Log("r");
+            Debug.Log(r);
+            Vector3 resV = r.MultiplyVector(Vector3.up);
+            Debug.Log("resV");
+            Debug.Log(resV);
+            Debug.Log(resV == testVNorm);
+            Debug.Log("\n\n");
+        }
     }
 
     // Returns a Matrix4x4 representing a rotation aligning the up direction of an object with the given v
     Matrix4x4 RotateTowardsVector(Vector3 v)
     {
         // Your code here
-        return Matrix4x4.zero;
+        Vector3 normV = v.normalized;
+        Debug.Log("normV is:");
+        Debug.Log(normV);
+        float thetaX = 90 - Mathf.Atan2(normV.y, normV.z) * Mathf.Rad2Deg;
+        Matrix4x4 rX = MatrixUtils.RotateX(-thetaX);
+        Vector3 rxV = rX * normV;
+        Debug.Log("The z coordinate should be 0:");
+        Debug.Log(rxV);
+        float thetaZ = 90 - Mathf.Atan2(rxV.y, rxV.x) * Mathf.Rad2Deg;
+        Matrix4x4 rZ = MatrixUtils.RotateZ(thetaZ);
+        Vector3 rzrxV = rZ * rxV;
+        Debug.Log("This vector should be (0,1,0):");
+        Debug.Log(rzrxV);
+        Matrix4x4 rXinv = rX.inverse;
+        Matrix4x4 rZinv = rZ.inverse;
+        Matrix4x4 rXIrZI = rXinv * rZinv;
+        return rXIrZI;
     }
 
     // Creates a Cylinder GameObject between two given points in 3D space
